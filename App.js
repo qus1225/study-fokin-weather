d5b975e939246ef6ee9985f5f5d52c09ef983662
Qus <qus1225@gmail.com>
2020-02-02 00:58:37 +0900

    프로젝트 셋팅

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello안녕하세요 ㅋㅋ !!!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    color: "#fff"
+  }
+});
