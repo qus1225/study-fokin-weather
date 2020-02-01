131dcee48779d081216f8d5078b20c90b1c8b7d7
Qus <qus1225@gmail.com>
2020-02-02 02:40:05 +0900

    온도 표시 하기

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import axios from "axios";
+import Weather from "./Weather";
 
 // TODO: 환경변수로 분리
 const API_KEY = "9cec7dea602ddedec48c5618a6775447";
@@ -13,9 +14,13 @@ export default class extends React.Component {
   };
   getWeather = async (latitude, longitude) => {
     const { data } = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
     );
     console.log(data);
+    this.setState({
+      isLoading: false,
+      temp: data.main.temp
+    });
   };
   getLocation = async () => {
     try {
@@ -36,7 +41,7 @@ export default class extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+    const { isLoading, temp } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
   }
 }
