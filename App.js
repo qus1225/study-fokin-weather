d22798752a57af94443b2da3cb4a73a004497bba
Qus <qus1225@gmail.com>
2020-02-02 01:20:26 +0900

    Loading 화면 추가

--- App.js
+++ App.js
@@ -1,29 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Loading from "./Loading";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      {/* <Text style={styles.text}>Hello안녕하세요 ㅋㅋ !!!</Text> */}
-      <View style={styles.yellowView} />
-      <View style={styles.blueView} />
-    </View>
-  );
+  return <Loading />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  // text: {
-  //   color: "#fff"
-  // }
-  yellowView: {
-    flex: 1,
-    backgroundColor: "yellow"
-  },
-  blueView: {
-    flex: 2,
-    backgroundColor: "blue"
-  }
-});
