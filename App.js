3c218a8f118f0065305392a2052afff130cd4f69
Qus <qus1225@gmail.com>
2020-02-02 02:25:09 +0900

    API로 현재 위치의 날씨 가져오기 추가

--- App.js
+++ App.js
@@ -2,10 +2,20 @@ import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+
+// TODO: 환경변수로 분리
+const API_KEY = "9cec7dea602ddedec48c5618a6775447";
 
 export default class extends React.Component {
   state = {
     isLoading: true
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
   };
   getLocation = async () => {
     try {
@@ -14,6 +24,7 @@ export default class extends React.Component {
         coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
       // TODO: Send API
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("Can't find you.", "So Sad");
